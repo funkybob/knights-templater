8ad95ada5e57ad941b1333cea8f8b81ce739a245
Curtis <curtis@tinbrain.net>
2015-03-24 23:28:58 +1100

    Convert to new style filter

--- knights/defaultfilters.py
+++ knights/defaultfilters.py
@@ -1,12 +1,10 @@
 
 from .library import Library
-from .filters import Filter
 
 
 register = Library()
 
 
-@register.filter(name='title')
-class TitleFilter(Filter):
-    def __rshift__(self, other):
-        return str(other).title()
+@register.filter
+def title(val):
+    return str(val).title()

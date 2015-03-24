040a7300f000b137b61896a8a73d829e24ee0c44
Curtis <curtis@tinbrain.net>
2015-03-24 23:46:04 +1100

    Add tests for comments and shifts that aren't for filters

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,3 +5,6 @@ class BasicFilterTest(TemplateTestCase):
 
     def test_basic_filter(self):
         self.assertRendered('{{ foo >> title }}', 'Bar', {'foo': 'bar'})
+
+    def test_not_filter(self):
+        self.assertRendered('{{ 8 >> 2 }}', '2')
--- tests/test_load.py
+++ tests/test_load.py
@@ -8,3 +8,10 @@ class LoadTagTest(TemplateTestCase):
     def test_load_default(self):
         t = Template('{! knights.defaultfilters !}')
         self.assertIn('title', t.parser.filters)
+
+
+
+class CommentTagText(TemplateTestCase):
+
+    def test_commend(self):
+        self.assertRendered('{# test #}', '')

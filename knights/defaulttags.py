37be1db6f770fc60d5b4426b8287e0c300d4982e
Curtis <curtis@tinbrain.net>
2015-03-26 22:07:31 +1100

    Add basic implementation of For loop

--- knights/defaulttags.py
+++ knights/defaulttags.py
@@ -5,9 +5,69 @@ import datetime
 
 register = Library()
 
+# autoescape
+# block
+# comment
+# csrf_token
+# cycle
+# debug
+# extends
+# filter
+# firstof
+# for
+
+@register.tag(name='for')
+class ForNode(BasicNode):
+    def __init__(self, parser, token):
+        super().__init__(parser, token)
+        self.nodelist = nodelist = []
+        # Collect nodes until we find a endfor
+        for node in parser.parse_node():
+            if isinstance(node, EndForNode):
+                break
+            self.nodelist.append(node)
+
+    def __call__(self, context):
+        args = [arg.id for arg in self.args]
+        kwargs = self.resolve_keywords(context)
+
+        source = kwargs['_in']
+        content = []
+        for item in iter(source):
+            # Update the context
+            if args:
+                ctx = dict(context, zip(args, item))
+            else:
+                ctx = dict(context, item=item)
+            # Render nodelist
+            content.extend(x(ctx) for x in self.nodelist)
+
+        return ''.join(content)
+
+@register.tag(name='endfor')
+class EndForNode(BasicNode):
+    pass
+
+# for ... empty
+# if
+# ifchanged
+# ifequal
+# ifnotequal
+# include
+# load
+# now
 
 @register.tag(name='now')
 class NowNode(BasicNode):
     def render(self, fmt):
         val = datetime.datetime.now()
         return val.strftime(fmt)
+
+# regroup
+# spaceless
+# ssi
+# templatetag
+# url
+# verbatim
+# widthratio
+# with
--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -5,3 +5,12 @@ class BlockTagTest(TemplateTestCase):
 
     def test_block_parse(self):
         self.assertRendered('{% now "%%" %}', '%')
+
+
+class ForTagTest(TemplateTestCase):
+    def test_simple_for(self):
+        self.assertRendered(
+            '{% for _in=seq %}{{ item }} {% endfor %}',
+            'a b c d e ',
+            {'seq': 'abcde'},
+        )

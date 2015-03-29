a7d10cd8138560009c889b13e2af95ec21fb391a
Curtis <curtis@tinbrain.net>
2015-03-29 20:00:28 +1100

    Reindent

--- knights/k_tags.py
+++ knights/k_tags.py
@@ -10,19 +10,21 @@ register = Library()
 def block(parser, token):
     token = token.strip()
     parser.build_method(token, endnodes=['endblock'])
-    return ast.Expr(value=ast.YieldFrom(
-        value=ast.Call(
-            func=ast.Attribute(
-                value=ast.Name(id='self', ctx=ast.Load()),
-                attr=token,
-                ctx=ast.Load()
-            ),
-            args=[
-                ast.Name(id='context', ctx=ast.Load()),
-            ],
-            keywords=[], starargs=None, kwargs=None
+    return ast.Expr(
+        value=ast.YieldFrom(
+            value=ast.Call(
+                func=ast.Attribute(
+                    value=ast.Name(id='self', ctx=ast.Load()),
+                    attr=token,
+                    ctx=ast.Load()
+                ),
+                args=[
+                    ast.Name(id='context', ctx=ast.Load()),
+                ],
+                keywords=[], starargs=None, kwargs=None
+            )
         )
-    ))
+    )
 
 
 @register.tag(name='if')

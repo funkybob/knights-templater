2bcacbbb5780f6361fef486dd10d454d338e7c53
Curtis <curtis@tinbrain.net>
2015-03-29 17:07:24 +1100

    Class-ify parsing

--- knights/klass.py
+++ knights/klass.py
@@ -1,7 +1,101 @@
 import ast
 from importlib import import_module
 
-from .lexer import Token, tokenise
+from .lexer import TokenType, tokenise
+
+
+class VarVisitor(ast.NodeTransformer):
+    def visit_Name(self, node):
+        return ast.Subscript(
+            value=ast.Name(id='context', ctx=ast.Load()),
+            slice=ast.Index(value=ast.Str(s=node.id)),
+            ctx=ast.Load(),
+        )
+
+
+class Parser:
+    def __init__(self, src):
+        self.stream = tokenise(src)
+        self.bases = ['object']
+        self.methods = []
+        self.tags = {}
+
+    def load_library(self, path):
+        '''
+        Load a template library into the state.
+        '''
+        module = import_module(path)
+        self.tags.update(module.register.tags)
+
+    def build_method(self, name, endnodes=None):
+
+        # Define the body of the function
+        body = [
+            # Include a blank to ensure it's never empty
+            ast.Expr(value=ast.Yield(value=ast.Str(s=''))),
+        ]
+
+        # Create the root method
+        func = ast.FunctionDef(
+            name=name,
+            args=ast.arguments(
+                args=[
+                    ast.arg(arg='self', annotation=None),
+                    ast.arg(arg='context', annotation=None),
+                ],
+                vararg=None,
+                kwonlyargs=[],
+                kwarg=None,
+                defaults=[],
+                kw_defaults=[],
+            ),
+            body=body,
+            decorator_list=[],
+        )
+
+        body.extend(self.parse_node(endnodes))
+
+        self.methods.append(func)
+
+        return func
+
+    def parse_node(self, endnodes=None):
+        for token in self.stream:
+            if token.mode == TokenType.load:
+                self.load_library(token.token)
+                continue
+            elif token.mode == TokenType.text:
+                node = ast.Yield(value=ast.Str(s=token.token))
+            elif token.mode == TokenType.var:
+                code = ast.parse(token.token, mode='eval')
+                VarVisitor().visit(code)
+                node = ast.Yield(value=code.body)
+            elif token.mode == TokenType.block:
+                bits = token.token.strip().split(' ', 1)
+                tag_name = bits.pop(0).strip()
+                if endnodes and tag_name in endnodes:
+                    return token
+                func = self.tags[tag_name]
+                node = func(self, *bits)
+            else:
+                # Must be a comment
+                continue
+
+            yield ast.Expr(value=node, lineno=token.lineno)
+
+    def build_class(self):
+        return ast.ClassDef(
+            name='Template',
+            bases=[
+                ast.Name(id=base, ctx=ast.Load())
+                for base in reversed(self.bases)
+            ],
+            body=self.methods,
+            keywords=[],
+            starargs=None,
+            kwargs=None,
+            decorator_list=[]
+        )
 
 
 def kompile(src):
@@ -22,12 +116,8 @@ def kompile(src):
 
     '''
 
-    state = {
-        'bases': ['object'],
-        'methods': [],
-        'stream': tokenise(src),
-        'tags': {},
-    }
+    parser = Parser(src)
+    parser.load_library('knights.k_tags')
 
     # Define the __call__ method
     func = ast.FunctionDef(
@@ -85,23 +175,10 @@ def kompile(src):
         decorator_list=[],
     )
 
-    state['methods'].append(func)
+    parser.methods.append(func)
+    parser.build_method('_root')
 
-    state['methods'].append(build_method(state, '_root'))
-
-    # define the class
-    klass = ast.ClassDef(
-        name='Template',
-        bases=[
-            ast.Name(id=base, ctx=ast.Load())
-            for base in reversed(state['bases'])
-        ],
-        body=state['methods'],
-        keywords=[],
-        starargs=None,
-        kwargs=None,
-        decorator_list=[]
-    )
+    klass = parser.build_class()
 
     # Wrap it in a module
     inst = ast.Module(body=[klass])
@@ -116,78 +193,3 @@ def kompile(src):
     eval(code, g)
 
     return g['Template']
-
-
-def build_method(state, name, endnode=None):
-
-    # Define the body of the function
-    body = [
-        # Include a blank to ensure it's never empty
-        ast.Expr(value=ast.Yield(value=ast.Str(s=''))),
-    ]
-
-    # Create the root method
-    func = ast.FunctionDef(
-        name=name,
-        args=ast.arguments(
-            args=[
-                ast.arg(arg='self', annotation=None),
-                ast.arg(arg='context', annotation=None),
-            ],
-            vararg=None,
-            kwonlyargs=[],
-            kwarg=None,
-            defaults=[],
-            kw_defaults=[],
-        ),
-        body=body,
-        decorator_list=[],
-    )
-
-    # Parse Nodes
-    # They need the parser, and state
-    body.extend(parse_node(state, endnode))
-
-    return func
-
-
-class VarVisitor(ast.NodeTransformer):
-    def visit_Name(self, node):
-        return ast.Subscript(
-            value=ast.Name(id='context', ctx=ast.Load()),
-            slice=ast.Index(value=ast.Str(s=node.id)),
-            ctx=ast.Load(),
-        )
-
-
-def parse_node(state, endnode=None):
-    for mode, token in state['stream']:
-        if mode == Token.load:
-            load_library(state, token)
-            continue
-        elif mode == Token.text:
-            node = ast.Yield(value=ast.Str(s=token))
-        elif mode == Token.var:
-            code = ast.parse(token, mode='eval')
-            VarVisitor().visit(code)
-            node = ast.Yield(value=code.body)
-        elif mode == Token.block:
-            bits = token.strip().split(' ', 1)
-            tag_name = bits.pop(0).strip()
-            if tag_name == endnode:
-                return
-            func = state['tags'][tag_name]
-            node = func(state, *bits)
-        else:
-            # Must be a comment
-            continue
-
-        yield ast.Expr(value=node)
-
-
-def load_library(state, path):
-    '''
-    Load a template library into the state.
-    '''
-    module = import_module(path)
-    state['tags'].update(module.register.tags)

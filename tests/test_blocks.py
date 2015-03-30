497be50549e9c7b3a886a1d0753386d8f93cea2b
Curtis Maloney <curtis@tinbrain.net>
2015-03-31 10:20:00 +1100

    Update tags for new syntax

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -10,14 +10,14 @@ class BlockTagTest(TemplateTestCase):
 class ForTagTest(TemplateTestCase):
     def test_simple_for(self):
         self.assertRendered(
-            '{% for _in=seq %}{{ item }} {% endfor %}',
+            '{% for item in seq %}{{ item }} {% endfor %}',
             'a b c d e ',
             {'seq': 'abcde'},
         )
 
     def test_unpack_for(self):
         self.assertRendered(
-            '{% for a, b, _in=seq %}{{ a }} == {{ b }},{% endfor %}',
+            '{% for a, b in seq %}{{ a }} == {{ b }},{% endfor %}',
             'a == 1,b == 2,',
             {'seq': (('a', 1), ('b', 2))}
         )

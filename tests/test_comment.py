cecd3fbd5a977f366ce5adad56c01b02a145b038
Curtis Maloney <curtis@tinbrain.net>
2015-03-30 14:06:09 +1100

    Remove dud test

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,15 +1,5 @@
 from .utils import TemplateTestCase, Mock
 
-from knights import Template
-
-
-class LoadTagTest(TemplateTestCase):
-
-    def test_load_default(self):
-        t = Template('{! knights.defaultfilters !}')
-        self.assertIn('escape', t.parser.filters)
-
-
 
 class CommentTagText(TemplateTestCase):
 

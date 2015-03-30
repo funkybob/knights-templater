88a2d0d7808a75c0e85482ad5cab8dd1e3570027
Curtis Maloney <curtis@tinbrain.net>
2015-03-30 14:04:07 +1100

    Try to implement for

--- knights/tags.py
+++ knights/tags.py
@@ -1,6 +1,7 @@
 
 import ast
 
+from .parser import VarVisitor
 from .library import Library
 
 register = Library()
@@ -42,3 +43,30 @@ def do_if(parser, token):
 @register.tag(name='else')
 def do_else(parser, token=None):
     return ast.Yield(value=ast.Str(s=''))
+
+
+@register.tag(name='for')
+def do_for(parser, token):
+    '''
+    {% for a, b, c, _in=iterable %}
+
+    {% endfor %}
+    '''
+    args, kwargs = parser.parse_args(token)
+
+    src = None
+    for kw in kwargs:
+        print(kw.arg, kw.value)
+        if kw.arg == '_in':
+            src = kw.value
+            break
+        raise ValueError('Only _in accepted as keyword in for tag.')
+
+    for arg in args:
+        VarVisitor().visit(arg)
+
+    VarVisitor().visit(src)
+
+    nodelist = list(parser.parse_node(['endfor']))
+
+    return ast.For(target=args, iter=src, body=nodelist, orelse=[])

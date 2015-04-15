c0b5c66f88b371550b48711df14879d809a782f2
Curtis Maloney <curtis@tinbrain.net>
2015-04-15 15:56:33 +1000

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='knights-templater',
+    version='0.1',
+    packages=find_packages(),
+    author='Curtis Maloney',
+    author_email='curtis@tinbrain.net',
+    description='A simple AST-based Python Template engine.',
+    keywords='template',
+)

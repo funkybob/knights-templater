804ca1e1065dc765c5bf384f31967f4465622a38
Curtis <curtis@tinbrain.net>
2015-04-20 20:50:41 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 setup(
     name='knights-templater',
     version='1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests.*',)),
     author='Curtis Maloney',
     author_email='curtis@tinbrain.net',
     description='A simple AST-based Python Template engine.',
     keywords='template',
+    url='http://github.com/funkybob/knights-templater/',
 )

d2b56e8c1af28a91b82496f5dd10fdf853da688c
Curtis Maloney <curtis@tinbrain.net>
2015-04-23 23:10:04 +1000

    Add static helper for django compat

--- knights/compat/django.py
+++ knights/compat/django.py
@@ -1,6 +1,7 @@
 import ast
 
 from django.core.urlresolvers import reverse
+from django.utils.encoding import iri_to_uri
 
 import datetime
 
@@ -38,3 +39,14 @@ def url(name, *args, **kwargs):
         return reverse(name, args=args, kwargs=kwargs)
     except:
         return None
+
+
+@register.helper
+def static(filename):
+    try:
+        from django.conf import settings
+    except ImportError:
+        prefix = ''
+    else:
+        prefix = iri_to_uri(getattr(settings, name, ''))
+    return prefix

9d058688986838459edf9f6ec40fac04867e0c2c
Curtis Maloney <curtis@tinbrain.net>
2015-04-24 10:41:51 +1000

    Remove bodgy static tag Remove duplicate capfirst helper

--- knights/compat/django.py
+++ knights/compat/django.py
@@ -10,28 +10,10 @@ from knights.library import Library
 register = Library()
 
 
-@register.tag
-def static(parser, token):
-    src = parser.parse_expression(token)
-    return ast.Yield(value=ast.BinOp(
-        left=ast.Str(s='/static/%s'),
-        op=ast.Mod(),
-        right=src,
-    ))
-
-
-@register.helper
-def capfirst(value):
-    return value and value[0].upper() + value[1:]
-
-@register.helper
-def safe(value):
-    return str(value)
-
-
 @register.helper
 def now(fmt):
     return datetime.datetime.now().strftime(fmt)
+
 
 @register.helper
 def url(name, *args, **kwargs):

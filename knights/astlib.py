9a3380c6f67036b60a3aeb22efa6cae2bd03617d
Curtis Maloney <curtis@tinbrain.net>
2015-04-25 15:36:02 +1000

    Start making astlib py2/3 aware

--- knights/astlib.py
+++ knights/astlib.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import sys
 import ast
 '''
 Helpers for constructing AST in a less verbose manner.
@@ -7,6 +8,7 @@ Helpers for constructing AST in a less verbose manner.
 In the majority of cases they allow for positional arguments, and provide
 sane/common defaults
 '''
+PY2 = sys.version_info[0] == 2
 
 
 def arguments(args=[], vararg=None, kwonlyargs=[], kwarg=None, defaults=[],
@@ -19,6 +21,11 @@ def arguments(args=[], vararg=None, kwonlyargs=[], kwarg=None, defaults=[],
 
 
 def args(*names):
+    if PY2:
+        return [
+            Name(name, ctx=ast.Param())
+            for name in names
+        ]
     return [
         ast.arg(arg=name, annotation=None)
         for name in names
@@ -35,7 +42,7 @@ def Attribute(value, attr, ctx=None):
 def Name(id, ctx=None):
     if ctx is None:
         ctx = ast.Load()
-    return ast.Name(id=id, ctx=ctx)
+    return ast.Name(id=str(id), ctx=ctx)
 
 
 def Call(func, args=[], keywords=[], starargs=None, kwargs=None):
@@ -46,4 +53,23 @@ def Call(func, args=[], keywords=[], starargs=None, kwargs=None):
         keywords=keywords,
         starargs=starargs,
         kwargs=kwargs
+    )
+
+
+def With(context_expr, optional_vars=[], body=[]):
+    if PY2:
+        return ast.With(
+            context_expr=context_expr,
+            optional_vars=optional_vars,
+            body=body,
+        )
+
+    return ast.With(
+        items=[
+            ast.withitem(
+                context_expr=context_expr,
+                optional_vars=optional_vars,
+            ),
+        ],
+        body=body,
     )
